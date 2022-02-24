4c020eba27dfa10f1a7050ee6fbd8f54b4ae4788
manumitsacademy <manumits.info@gmail.com>
2022-02-24 18:51:16 +0530

    'deps'

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ var app =express();
 var port = process.env.PORT || 4500;
 console.log(port)
 app.use(express.static(__dirname+"/dist/dashboard/"))
+app.get("/",(req,res)=>{
+    res.send("<h1>HAIII..</h1>")
+})
 app.listen(port,()=>{console.log("server running on port number:"+port)})
