3763bec175f4236d0e576d3d7d102b2dfc256b38
manumitsacademy <manumits.info@gmail.com>
2022-02-24 13:11:30 +0530

    'herokuupload'

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require("express");
 var app =express();
 var port = process.env.PORT || 4500;
 console.log(port)
-app.use(express.static(__dirname+"/dist/edupolyfrontend/"))
+app.use(express.static(__dirname+"/dist/"))
 app.listen(port,()=>{console.log("server running on port:"+port)})
