bc26c8270050753d0f439851024abd2574b5f069
manumitsacademy <manumits.info@gmail.com>
2022-02-24 18:59:04 +0530

    'deps'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app =express();
 var port = process.env.PORT || 4500;
 console.log(port)
-app.use(express.static(__dirname+"/dist/dashboard/"))
+app.use(express.static(__dirname+"/public/"))
 app.get("/",(req,res)=>{
     res.send("<h1>HAIII..</h1>")
 })
